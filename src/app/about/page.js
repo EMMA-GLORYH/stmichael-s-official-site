2727ebc4605193d74dac45a770b29f2ceaf38e0b
Elijah Emmaneuel Hienwo <emmanuelhienwo@gmail.com>
2025-12-13 10:31:15 +0000

    Initial commit

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+﻿export default function Page() {
+  return (
+    <div className='min-h-screen p-8'>
+      <h1 className='text-3xl font-bold'>Coming Soon</h1>
+      <p className='mt-4'>This page is under construction.</p>
+    </div>
+  );
+}
